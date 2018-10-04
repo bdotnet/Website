b36421a2171664c95dd38f39f0de7cc58bbe19b1
Nish <nnish@live.com>
2018-10-04 18:58:15 +0530

    SubmitTalks

--- themes/Hugo Fresh/static/js/speaker.js
+++ themes/Hugo Fresh/static/js/speaker.js
@@ -3,18 +3,20 @@ $(document).ready(function(){
         var name = $("#speaker-name").val();
         var email = $("#speaker-email").val();
         var topic = $("#speaker-description").val();
+        var ip = $("#speaker-ip").val();
+
         var jsonData = {name: name, email: email, topic: topic};
         var postData = JSON.stringify(jsonData);
-        alert(postData);
-        /*
+       // alert(postData);
+        
         $.ajax({
-            url: "http://localhost",
+            url: "http://"+ip+"api/SubmitTalk?t=Talk proposal from :" + name +"&d=" + email + "/n" + topic,
             method: "post",
             data: postData,
             success: function(d)
             {
-
+                alert("Thanks. We will get back to you.");
             }
-        })*/
+        })
     });
 });
